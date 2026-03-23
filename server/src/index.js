24d080e95ce313bf98bc28f2687b1a6b20c47f9d
Win Sern Law <lawin1207@Mac.localdomain>
2026-03-24 01:42:28 +0800

    Fix wildcard route syntax for path-to-regexp v8

--- server/src/index.js
+++ server/src/index.js
@@ -33,7 +33,7 @@ app.use('/api/alerts', alertRoutes);
 // Serve React frontend in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../client/dist')));
-  app.get('*', (req, res) => {
+  app.get('/{*path}', (req, res) => {
     res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   });
 }
